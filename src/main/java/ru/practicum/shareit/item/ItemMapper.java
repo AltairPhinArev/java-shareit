6f46ace146d6693e95b28bdcb6a3fe0faacdca78
Andrey Panasiuk <andreypnick@gmail.com>
2023-08-09 14:40:07 +0300

    refactor: code works on default with postgeSQL Server/style-1

--- src/main/java/ru/practicum/shareit/item/ItemMapper.java
+++ src/main/java/ru/practicum/shareit/item/ItemMapper.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Component;
 import ru.practicum.shareit.booking.BookingService;
 import ru.practicum.shareit.booking.dto.ShortBookingDto;
 import ru.practicum.shareit.item.comment.dto.CommentDto;
-import ru.practicum.shareit.item.comment.model.Comment;
 import ru.practicum.shareit.item.dto.ItemDto;
 import ru.practicum.shareit.item.dto.ItemDtoFull;
 import ru.practicum.shareit.item.model.Item;
