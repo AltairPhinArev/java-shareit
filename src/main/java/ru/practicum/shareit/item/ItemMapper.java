b5e36724e159d93bf8a40debc9abb94e1f55a959
Andrey Panasiuk <andreypnick@gmail.com>
2023-08-10 13:32:45 +0300

    refactor: added to model name of Columns and removed stream from if in update method of user, removed try/catch from itemCreate method/style

--- src/main/java/ru/practicum/shareit/item/ItemMapper.java
+++ src/main/java/ru/practicum/shareit/item/ItemMapper.java
@@ -1,9 +1,6 @@
 package ru.practicum.shareit.item;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
-import ru.practicum.shareit.booking.BookingService;
 import ru.practicum.shareit.booking.dto.ShortBookingDto;
 import ru.practicum.shareit.item.comment.dto.CommentDto;
 import ru.practicum.shareit.item.dto.ItemDto;
