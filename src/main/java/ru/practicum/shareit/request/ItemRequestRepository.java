79343ee9681a370635305f35ca71e4363b20bab8
Andrey Panasiuk <andreypnick@gmail.com>
2023-08-15 19:45:34 +0300

    refactor: add-item-request /styled-1

--- src/main/java/ru/practicum/shareit/request/ItemRequestRepository.java
+++ src/main/java/ru/practicum/shareit/request/ItemRequestRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import ru.practicum.shareit.request.model.ItemRequest;
 
 import java.util.List;
-import java.util.Optional;
+
 
 public interface ItemRequestRepository extends JpaRepository<ItemRequest, Long> {
 
