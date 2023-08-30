585a908f17847b0b6d86c8bdeedd8715378bad4d
Andrey Panasiuk <andreypnick@gmail.com>
2023-08-30 11:41:55 +0300

    add-docker

---
+++ gateway/src/main/java/ru.practicum.shareit/item/dto/ItemDto.java
@@ -0,0 +1,31 @@
+package ru.practicum.shareit.item.dto;
+
+import lombok.Builder;
+import lombok.Data;
+import ru.practicum.shareit.user.model.User;
+
+@Data
+public class ItemDto {
+
+    Long id;
+
+    String name;
+
+    String description;
+
+    Boolean available;
+
+    User owner;
+
+    Long requestId;
+
+    @Builder
+    public ItemDto(Long id, String name, String description, Boolean available, User owner, Long requestId) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.available = available;
+        this.owner = owner;
+        this.requestId = requestId;
+    }
+}
---
+++ gateway/src/main/java/ru.practicum.shareit/itemRequest/dto/ItemRequestDto.java
@@ -0,0 +1,32 @@
+package ru.practicum.shareit.itemRequest.dto;
+
+import lombok.Builder;
+import lombok.Data;
+import ru.practicum.shareit.item.dto.ItemDto;
+import ru.practicum.shareit.user.model.User;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+public class ItemRequestDto {
+
+     Long id;
+
+     String description;
+
+     User requester;
+
+     LocalDateTime created;
+
+     List<ItemDto> items;
+
+     @Builder
+     public ItemRequestDto(Long id, String description, User requester, LocalDateTime created, List<ItemDto> items) {
+          this.id = id;
+          this.description = description;
+          this.requester = requester;
+          this.created = created;
+          this.items = items;
+     }
+}
